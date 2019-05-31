fddb08940df60fca64e94a6e2255e5234494f98b
Kirill M <kalitkahlopal@gmail.com>
2019-06-01 00:54:37 +0300

    DZ7_upd

--- JS/JS_DZ7/js7.js
+++ JS/JS_DZ7/js7.js
@@ -10,25 +10,23 @@ span2.style.color = "green";
 let span3 = document.createElement("span");
 span3.style.color = "red";
 
+clock.appendChild(span1);
+clock.appendChild(span2);
+clock.appendChild(span3);
 
 function displayClock() {
     let date = new Date();
     let hrs = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-   
+
     hrs = (hrs < 10) ? '0' + hrs : hrs;
     min = (min < 10) ? '0' + min : min;
     sec = (sec < 10) ? '0' + sec : sec;
-    
-    clock.appendChild(span1);
-    clock.appendChild(span2);
-    clock.appendChild(span3);
 
     span1.innerHTML = hrs + ":";
     span2.innerHTML = min + ":";
     span3.innerHTML = sec;
-    
 
 }
 
