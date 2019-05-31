7613498b7726932036d41f0dea604b108546b2bc
Kirill M <kalitkahlopal@gmail.com>
2019-06-01 00:26:20 +0300

    DZ7

---
+++ JS/JS_DZ7/js7.js
@@ -0,0 +1,35 @@
+clock.style.width = "900px";
+clock.style.margin = "150px";
+clock.style.textAlign = "center";
+clock.style.fontSize = "150px";
+
+let span1 = document.createElement("span");
+span1.style.color = "orange";
+let span2 = document.createElement("span");
+span2.style.color = "green";
+let span3 = document.createElement("span");
+span3.style.color = "red";
+
+
+function displayClock() {
+    let date = new Date();
+    let hrs = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+   
+    hrs = (hrs < 10) ? '0' + hrs : hrs;
+    min = (min < 10) ? '0' + min : min;
+    sec = (sec < 10) ? '0' + sec : sec;
+    
+    clock.appendChild(span1);
+    clock.appendChild(span2);
+    clock.appendChild(span3);
+
+    span1.innerHTML = hrs + ":";
+    span2.innerHTML = min + ":";
+    span3.innerHTML = sec;
+    
+
+}
+
+setInterval(displayClock, 1000);
